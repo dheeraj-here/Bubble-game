f103a68080279c5b36bee83ff35c95ea5998f119
Dheeraj <bagwandheeraj20@gmail.com>
2023-10-16 08:51:56 +0530

    First Commit

---
+++ src/Components/Top.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../index.css'
+import { useRef } from 'react'
+
+const Top = (props) => {
+  return (
+    <div className='mx-7 rounded-md h-16 py-3 px-6 bg-green-700 flex justify-between'>
+      
+
+      <div className='head text-white text-2xl font-semibold'>Bubbling Game</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Top
